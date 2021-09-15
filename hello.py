daa545e6029e409e78fc9ec54720de3b391e16f8
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2021-09-15 15:19:41 +0000

    update

---
+++ hello.py
@@ -0,0 +1,13 @@
+import fire
+
+def hello(name="World"):
+  return "Hello %s!" % name
+class Calculator(object):
+  """A simple calculator class."""
+
+  def double(self, number):
+    return 2 * number
+
+if __name__ == '__main__':
+  fire.Fire(hello)
+  fire.Fire(Calculator)
